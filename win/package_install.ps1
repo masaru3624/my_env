54941e863edbb38aa3ff3e7a6dc948c151e996ab
Kosuke Miyagawa <kosuke.miyagawa.aj@renesas.com>
2024-11-26 19:13:57 +0900

    my first commit

---
+++ win/package_install.ps1
@@ -0,0 +1,18 @@
+echo "Hello Script!!"
+choco install everything
+choco install virtualbox
+choco install autohotkey
+# choco install xming
+choco install vcxsrv
+choco install winscp
+choco install teraterm
+# choco install powertoys
+choco install ninja
+choco install python3
+choco install vscode
+choco install rsync
+choco install winmerge
+choco install git
+choco install llvm
+choco install win32yank
+choco install neovim
